3fcb5bd3896bdd9950fdcb7f952661ad9c676a95
Tyler Tidman <tyler.tidman@draak.ca>
2017-12-27 16:20:50 -0500

    Also fetch the md5 file for checking arch linux arm downloads

--- arch_rpi.py
+++ arch_rpi.py
@@ -4,7 +4,9 @@ from fetcher import fetch_file
 
 
 if __name__ == '__main__':
-    fetch_file('https://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz.sig',
+    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz.sig',
                'ArchLinuxARM-rpi-3-latest.tar.gz.sig')
-    fetch_file('https://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz',
+    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz.md5',
+               'ArchLinuxARM-rpi-3-latest.tar.gz.md5')
+    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz',
                'ArchLinuxARM-rpi-3-latest.tar.gz')
