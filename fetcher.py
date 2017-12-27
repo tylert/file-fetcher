a3d160c55a9360dc5b97fc7fbd2d7c1aa716216b
Tyler Tidman <tyler.tidman@draak.ca>
2017-12-27 06:10:10 -0500

    Finish moving disto specific stuff back into each helper script

--- fetcher.py
+++ fetcher.py
@@ -1,8 +1,6 @@
 # XXX TODO Convert print to logging
 from __future__ import print_function
 
-import os
-
 # pip install semver
 import semver
 
@@ -33,25 +31,3 @@ def fetch_file(url, output):
     with open(output, 'wb') as outfile:
         for block in r.iter_content(1024):
             outfile.write(block)
-
-
-def fetch_ubuntu_release_files(release, iso):
-    '''Fetch an Ubuntu release.'''
-
-    fetch_file('http://releases.ubuntu.com/{}/SHA256SUMS'.format(release),
-               'SHA256SUMS-{}.txt'.format(os.path.splitext(iso)[0]))
-    fetch_file('http://releases.ubuntu.com/{}/SHA256SUMS.gpg'.format(release),
-               'SHA256SUMS-{}.gpg'.format(os.path.splitext(iso)[0]))
-    fetch_file('http://releases.ubuntu.com/{}/{}'.format(release, iso),
-               '{}'.format(iso))
-
-
-def fetch_ubuntu_testing_files(iso):
-    '''Fetch an Ubuntu testing release.'''
-
-    fetch_file('http://cdimage.ubuntu.com/ubuntu-server/daily/current/SHA256SUMS',
-               'SHA256SUMS-{}.txt'.format(os.path.splitext(iso)[0]))
-    fetch_file('http://cdimage.ubuntu.com/ubuntu-server/daily/current/SHA256SUMS.gpg',
-               'SHA256SUMS-{}.gpg'.format(os.path.splitext(iso)[0]))
-    fetch_file('http://cdimage.ubuntu.com/ubuntu-server/daily/current/{}'.format(iso),
-               '{}'.format(iso))
--- ubuntu.py
+++ ubuntu.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
+import os
+
 # pip install requests
 import requests
 
 # pip install beautifulsoup4
 from bs4 import BeautifulSoup
 
-from fetcher import (fetch_ubuntu_release_files,
-                     fetch_ubuntu_testing_files)
+from fetcher import fetch_file
+
+
+def fetch_ubuntu_release_files(release, iso):
+    '''Fetch an Ubuntu release.'''
+
+    fetch_file('http://releases.ubuntu.com/{}/SHA256SUMS'.format(release),
+               'SHA256SUMS-{}.txt'.format(os.path.splitext(iso)[0]))
+    fetch_file('http://releases.ubuntu.com/{}/SHA256SUMS.gpg'.format(release),
+               'SHA256SUMS-{}.gpg'.format(os.path.splitext(iso)[0]))
+    fetch_file('http://releases.ubuntu.com/{}/{}'.format(release, iso),
+               '{}'.format(iso))
+
+
+def fetch_ubuntu_testing_files(iso):
+    '''Fetch an Ubuntu testing release.'''
+
+    fetch_file('http://cdimage.ubuntu.com/ubuntu-server/daily/current/SHA256SUMS',
+               'SHA256SUMS-{}.txt'.format(os.path.splitext(iso)[0]))
+    fetch_file('http://cdimage.ubuntu.com/ubuntu-server/daily/current/SHA256SUMS.gpg',
+               'SHA256SUMS-{}.gpg'.format(os.path.splitext(iso)[0]))
+    fetch_file('http://cdimage.ubuntu.com/ubuntu-server/daily/current/{}'.format(iso),
+               '{}'.format(iso))
 
 
 if __name__ == '__main__':
