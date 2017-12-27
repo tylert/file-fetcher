1f06266a07846c1a0700b66165b35089cacdb01d
Tyler Tidman <tyler.tidman@draak.ca>
2017-12-27 14:47:50 -0500

    Add fetcher for FreeNAS too

--- arch.py
+++ arch.py
@@ -12,8 +12,10 @@ from fetcher import fetch_file
 def fetch_arch_release_files(iso):
     '''Fetch an Arch release.'''
 
-    fetch_file('https://mirror.cedille.club/archlinux/iso/latest/{}.sig'.format(iso), '{}.sig'.format(iso))
-    fetch_file('https://mirror.cedille.club/archlinux/iso/latest/{}'.format(iso), '{}'.format(iso))
+    fetch_file('https://mirror.cedille.club/archlinux/iso/latest/{}.sig'.format(iso),
+               '{}.sig'.format(iso))
+    fetch_file('https://mirror.cedille.club/archlinux/iso/latest/{}'.format(iso),
+               '{}'.format(iso))
 
 
 if __name__ == '__main__':
