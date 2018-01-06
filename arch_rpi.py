4f39ed4c206bca6ae07558063937fc750b395201
Tyler Tidman <tyler.tidman@draak.ca>
2018-01-05 19:36:47 -0500

    Update the full and bare requirements and date stamp the arch linux arm stuff

--- arch_rpi.py
+++ arch_rpi.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+import datetime
+
 from fetcher import fetch_file
 
 
 if __name__ == '__main__':
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+
     fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz.sig',
-               'ArchLinuxARM-rpi-3-latest.tar.gz.sig')
+               'ArchLinuxARM-rpi-3-{}.tar.gz.sig'.format(date))
     fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz.md5',
-               'ArchLinuxARM-rpi-3-latest.tar.gz.md5')
+               'ArchLinuxARM-rpi-3-{}.tar.gz.md5'.format(date))
     fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz',
-               'ArchLinuxARM-rpi-3-latest.tar.gz')
+               'ArchLinuxARM-rpi-3-{}.tar.gz'.format(date))
