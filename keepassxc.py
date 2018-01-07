54a5d4d8dc08d484864522c0adbce28510318a37
Tyler Tidman <tyler.tidman@draak.ca>
2018-01-06 22:33:55 -0500

    Add keepassxc fetcher

---
+++ keepassxc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re
+
+# pip install requests
+import requests
+
+# pip install beautifulsoup4
+from bs4 import BeautifulSoup
+
+from fetcher import fetch_file
+
+
+if __name__ == '__main__':
+    r = requests.get('https://github.com/keepassxreboot/keepassxc/releases/latest')
+    r.raise_for_status()
+    s = BeautifulSoup(r.text, 'html.parser')
+
+    for link in s.find_all('a'):
+        if 'tar.xz' in link.text and 'sig' not in link.text and 'DIGEST' not in link.text:
+            matcher = re.compile('\d+\.\d+\.\d+')
+            version = matcher.findall(link.text.split('\n')[3])[0]
+
+    fetch_file('https://github.com/keepassxreboot/keepassxc/releases/download/{}/keepassxc-{}-src.tar.xz.sig'.format(version, version),
+               'keepassxc-{}-src.tar.xz.sig'.format(version))
+    fetch_file('https://github.com/keepassxreboot/keepassxc/releases/download/{}/keepassxc-{}-src.tar.xz'.format(version, version),
+               'keepassxc-{}-src.tar.xz'.format(version))
