3001332b39ea2d5fbe6a15434a00f07542d03475
Tyler Tidman <tyler.tidman@draak.ca>
2018-06-28 17:38:49 -0400

    Cull Ubuntu 17.10 as it is EoL

--- ubuntu.py
+++ ubuntu.py
@@ -54,21 +54,6 @@ def main():
             iso = link.text
     fetch_ubuntu_release_files('bionic', iso)
 
-    # artful 17.10.x (EoL is 2018-07-01)
-    r = requests.get('http://releases.ubuntu.com/artful')
-    r.raise_for_status()
-    s = BeautifulSoup(r.text, 'html.parser')
-
-    for link in s.find_all('a'):
-        if 'server' in link.text and 'iso' in link.text and 'amd64' in link.text and 'torrent' not in link.text and 'zsync' not in link.text:
-            iso = link.text
-    fetch_ubuntu_release_files('artful', iso)
-
-    for link in s.find_all('a'):
-        if 'desktop' in link.text and 'iso' in link.text and 'amd64' in link.text and 'torrent' not in link.text and 'zsync' not in link.text:
-            iso = link.text
-    fetch_ubuntu_release_files('artful', iso)
-
     # xenial 16.04.x (EoL is 2021-04-01)
     r = requests.get('http://releases.ubuntu.com/xenial')
     r.raise_for_status()
