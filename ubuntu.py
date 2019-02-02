046bc12953dcb9a732c1b3f77996ed9dd2b0fcfc
Tyler Tidman <tyler.tidman@draak.ca>
2019-02-02 17:50:50 -0500

    Use the named release location for bionic

--- ubuntu.py
+++ ubuntu.py
@@ -26,18 +26,18 @@ def main():
     '''Main function.'''
 
     # bionic 18.04.x (EoL is 2023-04-01)
-    r = requests.get('http://cdimage.ubuntu.com/releases/18.04.1/release')
+    r = requests.get('http://cdimage.ubuntu.com/releases/bionic/release')
     r.raise_for_status()
     s = BeautifulSoup(r.text, 'html.parser')
 
     for link in s.find_all('a'):
         if 'server' in link.text and 'iso' in link.text and 'amd64' in link.text and 'torrent' not in link.text and 'zsync' not in link.text:
             iso = link.text
-    fetch_file('http://cdimage.ubuntu.com/releases/18.04.1/release/SHA256SUMS',
+    fetch_file('http://cdimage.ubuntu.com/releases/bionic/release/SHA256SUMS',
                'SHA256SUMS-{}.txt'.format(os.path.splitext(iso)[0]))
-    fetch_file('http://cdimage.ubuntu.com/releases/18.04.1/release/SHA256SUMS.gpg',
+    fetch_file('http://cdimage.ubuntu.com/releases/bionic/release/SHA256SUMS.gpg',
                'SHA256SUMS-{}.gpg'.format(os.path.splitext(iso)[0]))
-    fetch_file('http://cdimage.ubuntu.com/releases/18.04.1/release/{}'.format(iso),
+    fetch_file('http://cdimage.ubuntu.com/releases/bionic/release/{}'.format(iso),
                '{}'.format(iso))
 
     r = requests.get('http://releases.ubuntu.com/bionic')
