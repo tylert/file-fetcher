d667550cbf07d46c88ec0b1dd01e342269be561e
Tyler Tidman <tyler.tidman@draak.ca>
2020-03-05 18:32:54 -0500

    Start adding some notes for manjaro fetching

---
+++ manjaro.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+# NOTES
+# https://osdn.net/projects/manjaro/storage/Manjaro-User-Guide.pdf
+#   -->
+# https://osdn.net/frs/redir.php?m=constant&f=%2Fstorage%2Fg%2Fm%2Fma%2Fmanjaro%2FManjaro-User-Guide.pdf
+
+# f=/storage/g/m/ma/manjaro/...
+
+# https://osdn.net/projects/manjaro-community/storage/cinnamon/
+#   -->
+# https://osdn.net/projects/manjaro-community/storage/cinnamon/19.0.1/manjaro-cinnamon-19.0.1-minimal-200228-linux54.iso
+# https://osdn.net/projects/manjaro-community/storage/cinnamon/19.0.1/manjaro-cinnamon-19.0.1-minimal-200228-linux54.iso.sha256
+# https://osdn.net/projects/manjaro-community/storage/cinnamon/19.0.1/manjaro-cinnamon-19.0.1-minimal-200228-linux54.iso.sig
+#   -->
+# https://osdn.net/frs/redir.php?m=constant&f=%2Fstorage%2Fg%2Fm%2Fma%2Fmanjaro-community%2Fcinnamon%2F19.0.1%2Fmanjaro-cinnamon-19.0.1-minimal-200228-linux54.iso
+# https://osdn.net/frs/redir.php?m=gigenet&f=%2Fstorage%2Fg%2Fm%2Fma%2Fmanjaro-community%2Fcinnamon%2F19.0.1%2Fmanjaro-cinnamon-19.0.1-minimal-200228-linux54.iso.sha256
+# https://osdn.net/frs/redir.php?m=xtom_us&f=%2Fstorage%2Fg%2Fm%2Fma%2Fmanjaro-community%2Fcinnamon%2F19.0.1%2Fmanjaro-cinnamon-19.0.1-minimal-200228-linux54.iso.sig
+
+# f=/storage/g/m/ma/manjaro-community/cinnamon/19.0.1/...
