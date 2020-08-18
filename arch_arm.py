1440d70078d3df747e2c97b43e3822cf01d4a95c
Tyler Tidman <tyler.tidman@draak.ca>
2020-08-18 15:43:18 -0400

    Update the Arch Linux ARM fetcher for Pi 4B

--- arch_arm.py
+++ arch_arm.py
@@ -10,6 +10,14 @@ def main():
 
     date = datetime.datetime.today().strftime('%Y-%m-%d')
 
+    # Pi 4B/3B aarch64 variant
+    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz.sig',
+               'ArchLinuxARM-rpi-aarch64-{}.tar.gz.sig'.format(date))
+    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz.md5',
+               'ArchLinuxARM-rpi-aarch64-{}.tar.gz.md5'.format(date))
+    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz',
+               'ArchLinuxARM-rpi-aarch64-{}.tar.gz'.format(date))
+
     # Odroid HC2/HC1/XU4/XU3
     fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-odroid-xu3-latest.tar.gz.sig',
                'ArchLinuxARM-odroid-xu3-{}.tar.gz.sig'.format(date))
@@ -17,22 +25,6 @@ def main():
                'ArchLinuxARM-odroid-xu3-{}.tar.gz.md5'.format(date))
     fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-odroid-xu3-latest.tar.gz',
                'ArchLinuxARM-odroid-xu3-{}.tar.gz'.format(date))
-
-    # Raspberry Pi 4B
-    # fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-4-latest.tar.gz.sig',
-    #            'ArchLinuxARM-rpi-4-{}.tar.gz.sig'.format(date))
-    # fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-4-latest.tar.gz.md5',
-    #            'ArchLinuxARM-rpi-4-{}.tar.gz.md5'.format(date))
-    # fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-4-latest.tar.gz',
-    #            'ArchLinuxARM-rpi-4-{}.tar.gz'.format(date))
-
-    # Raspberry Pi 3B
-    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz.sig',
-               'ArchLinuxARM-rpi-3-{}.tar.gz.sig'.format(date))
-    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz.md5',
-               'ArchLinuxARM-rpi-3-{}.tar.gz.md5'.format(date))
-    fetch_file('http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-3-latest.tar.gz',
-               'ArchLinuxARM-rpi-3-{}.tar.gz'.format(date))
 
 
 if __name__ == '__main__':
