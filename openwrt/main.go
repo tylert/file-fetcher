e1d415dd43b65688981214a2f0ec445a6fd1dbd5
Tyler Tidman <tyler.tidman@draak.ca>
2023-05-25 16:04:56 -0400

    Make the OpenWRT stuff even fancier

--- openwrt/main.go
+++ openwrt/main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
-	// "strings"
-	// "github.com/PuerkitoBio/goquery"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Release struct {
@@ -22,11 +23,36 @@ type Release struct {
 	TarballURL      string    `json:"tarball_url"`
 }
 
-// curl https://api.github.com/repos/openwrt/openwrt/releases/latest | jq '.tarball_url'
-// curl https://api.github.com/repos/openwrt/openwrt/releases/latest | jq '.name'
-// https://downloads.openwrt.org/releases/${VERSION}/targets/ath79/generic/openwrt-${VERSION}-ath79-generic-tplink_eap225-v3-{initramfs-kernel,squashrs-factory,squashfs-sysupgrade}.bin
-// https://downloads.openwrt.org/releases/${VERSION}/targets/ramips/mt7621/openwrt-${VERSION}-ramips-mt7621-ubnt_edgerouter-x-{initramfs-kernel,squashfs-sysupgrade}.bin
-// + sha256sums
+func dumpOne(url string) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatalf("Error loading HTTP response body.", err)
+	}
+
+	// XXX FIXME TODO  Extract the corresponding sha256sum along with the filename!!!
+	// <tr><td class="n">
+	// <a href="openwrt-22.03.5-ath79-generic-tplink_eap225-v3-initramfs-kernel.bin">tplink_eap225-v3-initramfs-kernel.bin</a>
+	// </td>
+	// <td class="sh">3b28c9bf308b38ccb95aadbd4c52d9c686b8af6ba9ad1b00694f7fedd1f7506f</td>
+
+	doc.Find("a").Each(func(index int, element *goquery.Selection) {
+		href, exists := element.Attr("href")
+		if exists {
+			if strings.Contains(href, "tplink_eap225-v3") || (strings.Contains(href, "ubnt_edgerouter-x") && !strings.Contains(href, "sfp")) {
+				fmt.Println(fmt.Sprintf("%s/%s", url, href))
+			}
+		}
+	})
+}
 
 func main() {
 	res, err := http.Get("https://api.github.com/repos/openwrt/openwrt/releases/latest")
@@ -45,6 +71,7 @@ func main() {
 	}
 
 	fmt.Println(fmt.Sprintf("%s", rel.TarballURL))
-	fmt.Println(fmt.Sprintf("https://downloads.openwrt.org/releases/%s/targets/ath79/generic/", rel.Name))
-	fmt.Println(fmt.Sprintf("https://downloads.openwrt.org/releases/%s/targets/ramips/mt7621/", rel.Name))
+	fmt.Println(fmt.Sprintf("	out=openwrt-%s-src.tar.gz", rel.Name))
+	dumpOne(fmt.Sprintf("https://downloads.openwrt.org/releases/%s/targets/ath79/generic", rel.Name))
+	dumpOne(fmt.Sprintf("https://downloads.openwrt.org/releases/%s/targets/ramips/mt7621", rel.Name))
 }
