434b86306fd460e60192166c1cc7d8687b8a418e
Tyler Tidman <tyler.tidman@draak.ca>
2023-06-05 21:33:18 -0400

    Initial stab at Rocky Linux too

--- rocky/main.go
+++ rocky/main.go
@@ -33,11 +33,11 @@ func dumpOne(url string) {
 	doc.Find("div.name a").Each(func(i int, s *goquery.Selection) {
 		href, ok := s.Attr("href")
 		if ok {
-			if strings.Contains(href, ".iso") && !strings.Contains(href, "-latest") {
+			if strings.Contains(href, ".iso") && !strings.Contains(href, ".CHECKSUM") && !strings.Contains(href, ".torrent") && !strings.Contains(href, "-latest") {
 				fmt.Println(fmt.Sprintf("%s/%s", url, href))
 				fmt.Println("	allow-overwrite=true")
 				fmt.Println("	auto-file-renaming=false")
-				fmt.Println("	dir=AlmaLinux")
+				fmt.Println("	dir=Rocky")
 				fmt.Println("	file-allocation=falloc")
 				if reg.FindString(href) != "" {
 					ver = reg.FindString(href)
@@ -49,23 +49,23 @@ func dumpOne(url string) {
 	doc.Find("div.name a").Each(func(i int, s *goquery.Selection) {
 		href, ok := s.Attr("href")
 		if ok {
-			if strings.Contains(href, "CHECKSUM") {
+			if strings.Contains(href, "CHECKSUM") && !strings.Contains(href, ".CHECKSUM") {
 				fmt.Println(fmt.Sprintf("%s/%s", url, href))
 				fmt.Println("	auto-file-renaming=false")
-				fmt.Println("	dir=AlmaLinux")
+				fmt.Println("	dir=Rocky")
 				fmt.Println("	file-allocation=falloc")
-				fmt.Println(fmt.Sprintf("	out=AlmaLinux-%s-x86_64-%s", ver, href))
+				fmt.Println(fmt.Sprintf("	out=Rocky-%s-x86_64-%s", ver, href))
 			}
 		}
 	})
 }
 
 func main() {
-	fmt.Println("# https://mirror.xenyth.net/almalinux")
-	fmt.Println("# https://mirrors.almalinux.org/isos.html")
-	fmt.Println("# https://almalinux.org")
-	fmt.Println("# https://en.wikipedia.org/wiki/AlmaLinux")
+	fmt.Println("# https://mirror.xenyth.net/rocky")
+	fmt.Println("# https://rockylinux.org/download")
+	fmt.Println("# https://rockylinux.org")
+	fmt.Println("# https://en.wikipedia.org/wiki/Rocky_Linux")
 
-	dumpOne("https://mirror.xenyth.net/almalinux/9/isos/x86_64")
-	dumpOne("https://mirror.xenyth.net/almalinux/8/isos/x86_64")
+	dumpOne("https://mirror.xenyth.net/rocky/9/isos/x86_64")
+	dumpOne("https://mirror.xenyth.net/rocky/8/isos/x86_64")
 }
