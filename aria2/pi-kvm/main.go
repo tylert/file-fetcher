0d1607add251581e3179b0a41a549ca22bd1cb04
Tyler Tidman <tyler.tidman@draak.ca>
2023-06-26 20:05:31 -0400

    Fix the naming of the PiKVM dirs

--- aria2/pi-kvm/main.go
+++ aria2/pi-kvm/main.go
@@ -39,7 +39,7 @@ func main() {
 		if ok {
 			if strings.Contains(href, "v2-hdmi-rpi4-") {
 				fmt.Println(href)
-				fmt.Println("	dir=Pi-KVM")
+				fmt.Println("	dir=PiKVM")
 				thingy := strings.Split(strings.ReplaceAll(href, "latest", now.Format("2006-01-02")), "/")
 				fmt.Println(fmt.Sprintf("	out=pikvm-%s", thingy[len(thingy)-1]))
 			} else if strings.Contains(href, ".img") {
@@ -51,9 +51,9 @@ func main() {
 
 	// Documentation
 	fmt.Println("https://github.com/pikvm/pikvm/archive/refs/heads/gh-pages.zip")
-	fmt.Println("	dir=Pi-KVM")
+	fmt.Println("	dir=PiKVM")
 	fmt.Println(fmt.Sprintf("	out=pikvm-handbook-%s.zip", now.Format("2006-01-02")))
 	fmt.Println("https://github.com/pikvm/pikvm/archive/refs/heads/master.zip")
-	fmt.Println("	dir=Pi-KVM")
+	fmt.Println("	dir=PiKVM")
 	fmt.Println(fmt.Sprintf("	out=pikvm-more-docs-%s.zip", now.Format("2006-01-02")))
 }
