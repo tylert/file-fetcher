78640604a509fdfc4a2cef0521f6394950f9c08a
Tyler Tidman <tyler.tidman@draak.ca>
2023-07-21 14:28:23 -0400

    Make the path handling more betterer

--- booya/keys.go
+++ booya/keys.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"crypto/rand"
 	"encoding/pem"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/bitfield/script"
 	"github.com/mikesmitty/edkey"
@@ -14,30 +16,37 @@ import (
 )
 
 func AgeKeypair() {
-	// XXX FIXME TODO  Check if the tools/files exist first!!!
+	// XXX FIXME TODO  Check if the files exist first!!!
 
 	// age-keygen 2>/dev/null | tail -1 | (umask 0077 && tee secret_key_age) | age-keygen -y - 2>/dev/null > public_key_age  # generate keypair
 	// cat secret_key_file | age -p > meh && mv meh secret_key_file  # set/change passphrase
 	// age-keygen -y secret_key_age > public_key_age  # recover public key
 
-	b1 := new(bytes.Buffer)
-	b2 := new(bytes.Buffer)
-	_, err := script.Exec("age-keygen").Last(1).Tee(b1).Exec("age-keygen -y -").Tee(b2).String()
-	if err != nil {
-		panic(err)
-	}
+	paths := strings.Split(os.Getenv("PATH"), ":")
+	for _, path := range paths {
+		if _, err := os.Stat(fmt.Sprintf("%s/age-keygen", path)); err == nil {
+			b1 := new(bytes.Buffer)
+			b2 := new(bytes.Buffer)
+			_, err := script.Exec("age-keygen").Last(1).Tee(b1).Exec("age-keygen -y -").Tee(b2).String()
+			if err != nil {
+				panic(err)
+			}
 
-	f1, err := os.OpenFile("secret_key_age", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
-	}
-	b1.WriteTo(f1)
+			f1, err := os.OpenFile("secret_key_age", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+			if err != nil {
+				panic(err)
+			}
+			b1.WriteTo(f1)
 
-	f2, err := os.OpenFile("public_key_age", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
-	if err != nil {
-		panic(err)
+			f2, err := os.OpenFile("public_key_age", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
+			if err != nil {
+				panic(err)
+			}
+			b2.WriteTo(f2)
+
+			break
+		}
 	}
-	b2.WriteTo(f2)
 }
 
 func SSHKeypair() {
@@ -63,45 +72,59 @@ func SSHKeypair() {
 }
 
 func WireguardKeypair() {
-	// XXX FIXME TODO  Check if the tools/files exist first!!!
+	// XXX FIXME TODO  Check if the files exist first!!!
 
 	// wg genkey | (umask 0077 && tee secret_key_wg) | wg pubkey > public_key_wg  # generate keypair
 	// cat secret_key_wg | wg pubkey > public_key_wg  # recover public key
 
-	b1 := new(bytes.Buffer)
-	b2 := new(bytes.Buffer)
-	_, err := script.Exec("wg genkey").Tee(b1).Exec("wg pubkey").Tee(b2).String()
-	if err != nil {
-		panic(err)
-	}
+	paths := strings.Split(os.Getenv("PATH"), ":")
+	for _, path := range paths {
+		if _, err := os.Stat(fmt.Sprintf("%s/wg", path)); err == nil {
+			b1 := new(bytes.Buffer)
+			b2 := new(bytes.Buffer)
+			_, err := script.Exec("wg genkey").Tee(b1).Exec("wg pubkey").Tee(b2).String()
+			if err != nil {
+				panic(err)
+			}
 
-	f1, err := os.OpenFile("secret_key_wg", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
-	}
-	b1.WriteTo(f1)
+			f1, err := os.OpenFile("secret_key_wg", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+			if err != nil {
+				panic(err)
+			}
+			b1.WriteTo(f1)
 
-	f2, err := os.OpenFile("public_key_wg", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
-	if err != nil {
-		panic(err)
+			f2, err := os.OpenFile("public_key_wg", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
+			if err != nil {
+				panic(err)
+			}
+			b2.WriteTo(f2)
+
+			break
+		}
 	}
-	b2.WriteTo(f2)
 }
 
 func WireguardPreSharedKey() {
-	// XXX FIXME TODO  Check if the tools/files exist first!!!
+	// XXX FIXME TODO  Check if the files exist first!!!
 
 	// (umask 0077 && wg genpsk > shared_key_wg)  # generate pre-shared key
 
-	b3 := new(bytes.Buffer)
-	_, err := script.Exec("wg genpsk").Tee(b3).String()
-	if err != nil {
-		panic(err)
-	}
+	paths := strings.Split(os.Getenv("PATH"), ":")
+	for _, path := range paths {
+		if _, err := os.Stat(fmt.Sprintf("%s/wg", path)); err == nil {
+			b3 := new(bytes.Buffer)
+			_, err := script.Exec("wg genpsk").Tee(b3).String()
+			if err != nil {
+				panic(err)
+			}
 
-	f3, err := os.OpenFile("shared_key_wg", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
-	if err != nil {
-		panic(err)
+			f3, err := os.OpenFile("shared_key_wg", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+			if err != nil {
+				panic(err)
+			}
+			b3.WriteTo(f3)
+
+			break
+		}
 	}
-	b3.WriteTo(f3)
 }
--- booya/python.go
+++ booya/python.go
@@ -12,28 +12,29 @@ import (
 func Python() {
 	// XXX FIXME TODO  Also try to find things named:  python2, python3, python37, python3.9, etc.
 	paths := strings.Split(os.Getenv("PATH"), ":")
-	for i := 0; i < len(paths); i++ {
-		if _, err := os.Stat(fmt.Sprintf("%s/python", paths[i])); err == nil {
-			script.Echo(fmt.Sprintf("This is the version of python installed at %s/python:\n", paths[i])).Stdout()
-			script.Exec(fmt.Sprintf("%s/python --version", paths[i])).Stdout()
+	for _, path := range paths {
+		if _, err := os.Stat(fmt.Sprintf("%s/python", path)); err == nil {
+			script.Echo(fmt.Sprintf("This is the version of python installed at %s/python:\n", path)).Stdout()
+			script.Exec(fmt.Sprintf("%s/python --version", path)).Stdout()
 			script.Echo("\n").Stdout()
 
 			script.Echo("This instance of python has the following 'site' module:\n").Stdout()
-			script.Exec(fmt.Sprintf("%s/python -m site", paths[i])).Stdout()
+			script.Exec(fmt.Sprintf("%s/python -m site", path)).Stdout()
 			script.Echo("\n").Stdout()
 		}
 	}
 }
 
 func Pyenv() {
+	// pyenv install --list | grep -E '\s3' | tail
+
 	paths := strings.Split(os.Getenv("PATH"), ":")
-	for i := 0; i < len(paths); i++ {
-		if _, err := os.Stat(fmt.Sprintf("%s/pyenv", paths[i])); err == nil {
+	for _, path := range paths {
+		if _, err := os.Stat(fmt.Sprintf("%s/pyenv", path)); err == nil {
 			script.Echo("You have this version of pyenv installed:\n").Stdout()
 			script.Exec("pyenv --version").Stdout()
 			script.Echo("\n").Stdout()
 
-			// pyenv install --list | grep -E '\s3' | tail
 			script.Echo("These are the newest versions of python available for your pyenv:\n").Stdout()
 			script.Exec("pyenv install --list").MatchRegexp(regexp.MustCompile(`\s3`)).Last(10).Stdout()
 			script.Echo("\n").Stdout()
