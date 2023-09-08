cb33b5883f3a73b009bbe6de1f8f14f362b8582b
Tyler Tidman <tyler.tidman@draak.ca>
2023-09-08 16:42:10 -0400

    Links to ThinkPad BIOS things don't seem to be working, try again

--- aria2/bios/main.go
+++ aria2/bios/main.go
@@ -48,9 +48,9 @@ func dumpOne(url string) {
 func doIt() {
 	dumpOne("https://www.asus.com/supportonly/rog%20strix%20b550-a%20gaming/helpdesk_bios")
 	dumpOne("https://www.asus.com/supportonly/rog%20strix%20b450-f%20gaming/helpdesk_bios")
-	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t560/downloads/driver-list/component?name=BIOS%2FUEFI
-	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t460/downloads/driver-list/component?name=BIOS%2FUEFI
-	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t450s/downloads/driver-list/component?name=BIOS%2FUEFI
+	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t560/downloads/driver-list/component?name=BIOS%2FUEFI&id=5AC6A815-321D-440E-8833-B07A93E0428C
+	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t460/downloads/driver-list/component?name=BIOS%2FUEFI&id=5AC6A815-321D-440E-8833-B07A93E0428C
+	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t450s/downloads/driver-list/component?name=BIOS%2FUEFI&id=5AC6A815-321D-440E-8833-B07A93E0428C
 	// https://www.cyberciti.biz/faq/update-lenovo-bios-from-linux-usb-stick-pen
 	// https://1832jsh.github.io/thinkpad/BIOS_logo.html
 	// https://wilkgr76.github.io/tp_logo
