b64199d26a6357814c932de7d8c15cc3ce936c13
Tyler Tidman <tyler.tidman@draak.ca>
2023-09-29 16:01:23 -0400

    Rename help to usage

--- booya/cli.go
+++ booya/cli.go
@@ -21,31 +21,31 @@ var (
 )
 
 func init() {
-	// Help for command-line arguments
+	// Usage for command-line arguments
 	const (
-		sAgeKey      = "Generate an age keypair (default false)"
-		sForce       = "Overwrite key files if they exist"
-		sMinisignKey = "Generate a minisign keypair (default false)"
-		sSshKey      = "Generate an ssh keypair (default false)"
-		sVersion     = "Display build version information (default false)"
-		sWgKey       = "Generate a wireguard keypair (default false)"
-		sWgPsk       = "Generate a wireguard pre-shared key (default false)"
+		uAgeKey      = "Generate an age keypair (default false)"
+		uForce       = "Overwrite key files if they exist"
+		uMinisignKey = "Generate a minisign keypair (default false)"
+		uSshKey      = "Generate an ssh keypair (default false)"
+		uVersion     = "Display build version information (default false)"
+		uWgKey       = "Generate a wireguard keypair (default false)"
+		uWgPsk       = "Generate a wireguard pre-shared key (default false)"
 	)
 
-	flag.BoolVar(&aAgeKey, "age", FromEnvP("MEH_AGE", false).(bool), sAgeKey)
-	flag.BoolVar(&aAgeKey, "a", FromEnvP("MEH_AGE", false).(bool), sAgeKey)
-	flag.BoolVar(&aForce, "force", FromEnvP("MEH_FORCE", false).(bool), sForce)
-	flag.BoolVar(&aForce, "f", FromEnvP("MEH_FORCE", false).(bool), sForce)
-	flag.BoolVar(&aMinisignKey, "minisign", FromEnvP("MEH_MINISIGN", false).(bool), sMinisignKey)
-	flag.BoolVar(&aMinisignKey, "m", FromEnvP("MEH_MINISIGN", false).(bool), sMinisignKey)
-	flag.BoolVar(&aSshKey, "ssh", FromEnvP("MEH_SSH", false).(bool), sSshKey)
-	flag.BoolVar(&aSshKey, "s", FromEnvP("MEH_SSH", false).(bool), sSshKey)
-	flag.BoolVar(&aVersion, "version", FromEnvP("MEH_VERSION", false).(bool), sVersion)
-	flag.BoolVar(&aVersion, "v", FromEnvP("MEH_VERSION", false).(bool), sVersion)
-	flag.BoolVar(&aWgKey, "wg", FromEnvP("MEH_WG", false).(bool), sWgKey)
-	flag.BoolVar(&aWgKey, "w", FromEnvP("MEH_WG", false).(bool), sWgKey)
-	flag.BoolVar(&aWgPsk, "wgpsk", FromEnvP("MEH_WGPSK", false).(bool), sWgPsk)
-	flag.BoolVar(&aWgPsk, "p", FromEnvP("MEH_WGPSK", false).(bool), sWgPsk)
+	flag.BoolVar(&aAgeKey, "age", FromEnvP("MEH_AGE", false).(bool), uAgeKey)
+	flag.BoolVar(&aAgeKey, "a", FromEnvP("MEH_AGE", false).(bool), uAgeKey)
+	flag.BoolVar(&aForce, "force", FromEnvP("MEH_FORCE", false).(bool), uForce)
+	flag.BoolVar(&aForce, "f", FromEnvP("MEH_FORCE", false).(bool), uForce)
+	flag.BoolVar(&aMinisignKey, "minisign", FromEnvP("MEH_MINISIGN", false).(bool), uMinisignKey)
+	flag.BoolVar(&aMinisignKey, "m", FromEnvP("MEH_MINISIGN", false).(bool), uMinisignKey)
+	flag.BoolVar(&aSshKey, "ssh", FromEnvP("MEH_SSH", false).(bool), uSshKey)
+	flag.BoolVar(&aSshKey, "s", FromEnvP("MEH_SSH", false).(bool), uSshKey)
+	flag.BoolVar(&aVersion, "version", FromEnvP("MEH_VERSION", false).(bool), uVersion)
+	flag.BoolVar(&aVersion, "v", FromEnvP("MEH_VERSION", false).(bool), uVersion)
+	flag.BoolVar(&aWgKey, "wg", FromEnvP("MEH_WG", false).(bool), uWgKey)
+	flag.BoolVar(&aWgKey, "w", FromEnvP("MEH_WG", false).(bool), uWgKey)
+	flag.BoolVar(&aWgPsk, "wgpsk", FromEnvP("MEH_WGPSK", false).(bool), uWgPsk)
+	flag.BoolVar(&aWgPsk, "p", FromEnvP("MEH_WGPSK", false).(bool), uWgPsk)
 	iniflags.Parse()
 
 	if flag.NArg() > 0 {
