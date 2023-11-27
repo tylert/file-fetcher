0d7496cc9e97dc2953dd8cf2248d18b6825ae01e
Tyler Tidman <tyler.tidman@draak.ca>
2023-11-27 16:13:58 -0500

    Scrape new version info about gojq too

---
+++ aria2/gojq/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type Release struct {
+	HTMLURL         string    `json:"html_url"`
+	TagName         string    `json:"tag_name"`
+	TargetCommitish string    `json:"target_commitish"`
+	Name            string    `json:"name"`
+	Draft           bool      `json:"draft"`
+	Prerelease      bool      `json:"prerelease"`
+	CreatedAt       time.Time `json:"created_at"`
+	PublishedAt     time.Time `json:"published_at"`
+	Assets          []struct {
+		Name               string    `json:"name"`
+		ContentType        string    `json:"content_type"`
+		Size               int       `json:"size"`
+		CreatedAt          time.Time `json:"created_at"`
+		UpdatedAt          time.Time `json:"updated_at"`
+		BrowserDownloadURL string    `json:"browser_download_url"`
+	} `json:"assets"`
+	TarballURL string `json:"tarball_url"`
+	ZipballURL string `json:"zipball_url"`
+}
+
+func doIt() {
+	res, err := http.Get("https://api.github.com/repos/itchyny/gojq/releases/latest")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	var rel Release
+	err = json.NewDecoder(res.Body).Decode(&rel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("# https://github.com/itchyny/gojq/releases")
+	fmt.Println("# https://github.com/itchyny/gojq")
+
+	// This project uses version strings that start with "v" in some places
+	reg := regexp.MustCompile(`\d+?\.\d+?\.\d+`)
+	ver := reg.FindString(rel.TagName)
+
+	// Compiled binaries
+	for i := 0; i < len(rel.Assets); i++ {
+		if strings.Contains(rel.Assets[i].Name, "linux_arm64") {
+			fmt.Println(rel.Assets[i].BrowserDownloadURL)
+			fmt.Println("	dir=gojq")
+			fmt.Println(fmt.Sprintf("	out=gojq-%s-linux-arm64.tar.gz", ver))
+		} else if strings.Contains(rel.Assets[i].Name, "linux_amd64") {
+			fmt.Println(rel.Assets[i].BrowserDownloadURL)
+			fmt.Println("	dir=gojq")
+			fmt.Println(fmt.Sprintf("	out=gojq-%s-linux-amd64.tar.gz", ver))
+		} else {
+			fmt.Println(fmt.Sprintf("# skipped %s", rel.Assets[i].Name))
+		}
+	}
+
+	// Source code
+	fmt.Println(rel.TarballURL)
+	fmt.Println("	dir=gojq")
+	fmt.Println(fmt.Sprintf("	out=gojq-%s-src.tar.gz", ver))
+}
+
+func main() {
+	doIt()
+}
