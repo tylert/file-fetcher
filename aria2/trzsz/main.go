8bcaf4f87a111dedbfdfa97d03aa4b06d9249397
Tyler Tidman <tyler.tidman@draak.ca>
2024-01-03 17:59:24 -0500

    Nab some trzsz tools as well

---
+++ aria2/trzsz/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type Release struct {
+	HTMLURL         string    `json:"html_url"`
+	TagName         string    `json:"tag_name"`
+	TargetCommitish string    `json:"target_commitish"`
+	Name            string    `json:"name"`
+	Draft           bool      `json:"draft"`
+	Prerelease      bool      `json:"prerelease"`
+	CreatedAt       time.Time `json:"created_at"`
+	PublishedAt     time.Time `json:"published_at"`
+	Assets          []struct {
+		Name               string    `json:"name"`
+		ContentType        string    `json:"content_type"`
+		Size               int       `json:"size"`
+		CreatedAt          time.Time `json:"created_at"`
+		UpdatedAt          time.Time `json:"updated_at"`
+		BrowserDownloadURL string    `json:"browser_download_url"`
+	} `json:"assets"`
+	TarballURL string `json:"tarball_url"`
+	ZipballURL string `json:"zipball_url"`
+}
+
+func doOne(url string, tool string) {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	var rel Release
+	err = json.NewDecoder(res.Body).Decode(&rel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// This project uses version strings that start with "v" in some places
+	reg := regexp.MustCompile(`\d+?\.\d+?\.\d+`)
+	ver := reg.FindString(rel.TagName)
+
+	// Compiled binaries
+	for i := 0; i < len(rel.Assets); i++ {
+		if (strings.Contains(rel.Assets[i].Name, "linux") || strings.Contains(rel.Assets[i].Name, "checksums")) && !strings.Contains(rel.Assets[i].Name, "386") && !strings.Contains(rel.Assets[i].Name, "rpm") {
+			fmt.Println(rel.Assets[i].BrowserDownloadURL)
+			fmt.Println("	dir=trzsz")
+		} else {
+			fmt.Println(fmt.Sprintf("# skipped %s", rel.Assets[i].Name))
+		}
+	}
+
+	// Source code
+	fmt.Println(rel.TarballURL)
+	fmt.Println("	dir=trzsz")
+	fmt.Println(fmt.Sprintf("	out=%s_%s_src.tar.gz", tool, ver))
+}
+
+func doIt() {
+	// Spit out some handy links
+	fmt.Println("# https://github.com/trzsz/trzsz-go/releases")
+	fmt.Println("# https://github.com/trzsz/trzsz-go")
+	fmt.Println("# https://github.com/trzsz/trzsz-ssh/releases")
+	fmt.Println("# https://github.com/trzsz/trzsz-ssh")
+	fmt.Println("# https://trzsz.github.io/go")
+	fmt.Println("# https://trzsz.github.io/ssh")
+	fmt.Println("# https://trzsz.github.io")
+	fmt.Println("# https://github.com/trzsz/trzsz.github.io")
+
+	doOne("https://api.github.com/repos/trzsz/trzsz-go/releases/latest", "trzsz")
+	doOne("https://api.github.com/repos/trzsz/trzsz-ssh/releases/latest", "tssh")
+}
+
+func main() {
+	doIt()
+}
