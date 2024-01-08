135eca2b95ea81fdc258a9668ab33cc7d04272de
Tyler Tidman <tyler.tidman@draak.ca>
2024-01-08 10:11:11 -0500

    Check if formatters are installed first

--- helper/main.go
+++ helper/main.go
@@ -21,14 +21,28 @@ func dedupeSlice[T comparable](sliceList []T) []T {
 }
 
 func main() {
+	// Work on entire subdirectories at a time
 	dirs, err := script.FindFiles(".").Dirname().Slice()
 	if err != nil {
 		panic(err)
 	}
 	dd := dedupeSlice(dirs)
 
-	fmt.Println("Formatting Go modules")
-	script.Slice(dd).ExecForEach("gofmt -l -w {{ . }}").Stdout()
-	// fmt.Println("Formatting Terraform modules")
-	// script.Slice(dd).ExecForEach("terraform fmt -list=true -write=true {{ . }}").Stdout()
+	// Format Go files
+	_, err2 := script.Exec("go version").String()
+	if err2 != nil {
+		panic(err2)
+	} else {
+		fmt.Println("Formatting Go modules")
+		script.Slice(dd).ExecForEach("gofmt -l -w {{ . }}").Stdout()
+	}
+
+	// Format Terraform files
+	_, err3 := script.Exec("terraform version").String()
+	if err3 != nil {
+		panic(err3)
+	} else {
+		fmt.Println("Formatting Terraform modules")
+		script.Slice(dd).ExecForEach("terraform fmt -list=true -write=true {{ . }}").Stdout()
+	}
 }
