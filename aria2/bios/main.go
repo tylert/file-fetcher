5123059668f354959af654f715c86bc5dfeaa7a3
Tyler Tidman <tyler.tidman@draak.ca>
2024-02-14 21:06:31 -0500

    Fix the wonky BIOS fetching links

--- aria2/bios/main.go
+++ aria2/bios/main.go
@@ -46,8 +46,8 @@ func dumpOne(url string) {
 }
 
 func doIt() {
-	dumpOne("https://www.asus.com/supportonly/rog%20strix%20b550-a%20gaming/helpdesk_bios")
-	dumpOne("https://www.asus.com/supportonly/rog%20strix%20b450-f%20gaming/helpdesk_bios")
+	dumpOne("https://www.asus.com/ca-en/supportonly/rog%20strix%20b550-a%20gaming/helpdesk_bios")
+	dumpOne("https://www.asus.com/ca-en/supportonly/rog%20strix%20b450-f%20gaming/helpdesk_bios")
 	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t560/downloads/driver-list/component?name=BIOS%2FUEFI&id=5AC6A815-321D-440E-8833-B07A93E0428C
 	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t460/downloads/driver-list/component?name=BIOS%2FUEFI&id=5AC6A815-321D-440E-8833-B07A93E0428C
 	// https://pcsupport.lenovo.com/ca/en/products/laptops-and-netbooks/thinkpad-t-series-laptops/thinkpad-t450s/downloads/driver-list/component?name=BIOS%2FUEFI&id=5AC6A815-321D-440E-8833-B07A93E0428C
