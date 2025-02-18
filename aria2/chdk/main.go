14347dc11ce30e119c3cfa2153d03a962caf6780
Tyler Tidman <tyler.tidman@draak.ca>
2025-02-18 17:52:49 -0500

    CHDK fetching isn't going to be so easy now

--- aria2/chdk/main.go
+++ aria2/chdk/main.go
@@ -9,53 +9,44 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func dumpOne(url string, target string) {
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatal("Error loading HTTP response body.", err)
-	}
-
-	fmt.Println(fmt.Sprintf("# %s", url))
-
-	// Break after we find a single match (e.g.:  device twins)
-	doc.Find("a").EachWithBreak(func(i int, s *goquery.Selection) bool {
-		href, ok := s.Attr("href")
-		if ok {
-			// Don't bother with the partial builds, only full ones
-			if strings.Contains(href, target) && strings.Contains(href, "full.zip") {
-				fmt.Println(fmt.Sprintf("%s/%s", url, href))
-				fmt.Println("	dir=CHDK")
-				return false
-			}
-		}
-		return true
-	})
-}
-
 func doIt() {
+	// XXX FIXME TODO  Sigh, the CHDK builds page is all Jabbascript... https://github.com/robertkrimen/otto
+	// https://github.com/PuerkitoBio/goquery/wiki/Tips-and-tricks#handle-javascript-based-pages
+	// https://gist.github.com/cryptix/87127f76a94183747b53
+
+	/*
+		res, err := http.Get("https://build.chdk.photos/#build=release/IXUS/ixus132_elph115/100b")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+		if res.StatusCode != 200 {
+			log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+		}
+
+			doc, err := goquery.NewDocumentFromReader(res.Body)
+			if err != nil {
+				log.Fatal("Error loading HTTP response body.", err)
+			}
+	*/
+
 	// Spit out some handy links
 	fmt.Println("# https://en.wikipedia.org/wiki/DIGIC#CHDK")
 	fmt.Println("# https://chdk.fandom.com/wiki/Downloads")
-	fmt.Println("# https://build.chdk.photos")
+	fmt.Println("# https://build.chdk.photos/#build=release/IXUS/ixus132_elph115/100b")
 	fmt.Println("# https://app.assembla.com/spaces/chdk/subversion/source")
 
-	// XXX FIXME TODO  This one is currently broken... make it work again!!!
-	// Go to https://build.chdk.photos and click on the following:
-	//     Development / Unstable (trunk)
-	//     Release / Stable (release)
-	//         -> Digital IXUS (SD, ELPH, IXY)
-	//             -> IXUS 132 (ELPH 115 IS, IXY 90F)
-	//                 -> 100b
-	//                     -> Complete build (zip link with inline sha256 checksum)
+	/*
+		doc.Find("a").Each(func(i int, s *goquery.Selection) {
+			href, ok := s.Attr("href")
+			if ok {
+				if strings.Contains(href, "full.zip") {
+					fmt.Println(fmt.Sprintf("%s", href))
+					fmt.Println("	dir=CHDK")
+				}
+			}
+		})
+	*/
 }
 
 func main() {
