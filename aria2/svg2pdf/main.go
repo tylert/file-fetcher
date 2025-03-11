02939ab5e31d97a2f3c03eaa3fd9ce199e0a4b4c
Tyler Tidman <tyler.tidman@draak.ca>
2025-03-11 14:03:47 -0400

    Add svg2pdf helper

---
+++ aria2/svg2pdf/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"time"
+)
+
+type Release struct {
+	HTMLURL         string    `json:"html_url"`
+	TagName         string    `json:"tag_name"`
+	TargetCommitish string    `json:"target_commitish"`
+	Name            string    `json:"name"`
+	Draft           bool      `json:"draft"`
+	Prerelease      bool      `json:"prerelease"`
+	CreatedAt       time.Time `json:"created_at"`
+	PublishedAt     time.Time `json:"published_at"`
+	Assets          []struct {
+		Name               string    `json:"name"`
+		ContentType        string    `json:"content_type"`
+		Size               int       `json:"size"`
+		CreatedAt          time.Time `json:"created_at"`
+		UpdatedAt          time.Time `json:"updated_at"`
+		BrowserDownloadURL string    `json:"browser_download_url"`
+	} `json:"assets"`
+	TarballURL string `json:"tarball_url"`
+	ZipballURL string `json:"zipball_url"`
+}
+
+func doIt() {
+	res, err := http.Get("https://api.github.com/repos/typst/svg2pdf/releases/latest")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	var rel Release
+	err = json.NewDecoder(res.Body).Decode(&rel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// This project uses version strings that start with "v" in some places
+	reg := regexp.MustCompile(`\d+?\.\d+?\.\d+`)
+	ver := reg.FindString(rel.TagName)
+
+	// Spit out some handy links
+	fmt.Println("# https://github.com/typst/svg2pdf/releases")
+	fmt.Println("# https://github.com/typst/svg2pdf")
+	fmt.Println("# https://typst.app")
+
+	// Source code
+	fmt.Println(rel.TarballURL)
+	fmt.Println("	dir=svg2pdf")
+	fmt.Println(fmt.Sprintf("	out=svg2pdf_%s_src.tar.gz", ver))
+}
+
+func main() {
+	doIt()
+}
