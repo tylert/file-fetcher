f6f8ea5bc5721a6e01f10dc96c6556ac32212cb9
Tyler Tidman <tyler.tidman@draak.ca>
2025-04-14 21:37:31 -0400

    Preliminary work to get WPSD fetching working

---
+++ aria2/wpsd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func doIt() {
+	res, err := http.Get("https://w0chp.radio/wpsd")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body.", err)
+	}
+
+	// Spit out some handy links
+	fmt.Println("# https://w0chp.radio/wpsd")
+  fmt.Println("# https://manual.wpsd.radio")
+
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		href, ok := s.Attr("href")
+		if ok {
+			if strings.Contains(href, "RPi") || strings.Contains(href, "SHA256") {
+				fmt.Println(fmt.Sprintf("%s", href))
+				fmt.Println("	dir=WPSD")
+			} else if strings.Contains(href, "WPSD") {
+				fmt.Println(fmt.Sprintf("# skipped %s", href))
+			}
+		}
+	})
+}
+
+func main() {
+	doIt()
+}
