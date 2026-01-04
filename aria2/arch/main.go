7132f0dd15ee4a8bede880e098a6cb24af5ac4b8
Tyler Tidman <tyler.tidman@draak.ca>
2026-01-04 15:40:33 -0500

    Tweak the Arch fetcher

--- aria2/arch/main.go
+++ aria2/arch/main.go
@@ -21,38 +21,20 @@ func Arch() {
 	// Spit out some handy links
 	fmt.Println("# https://mirror.xenyth.net/archlinux")
 	fmt.Println("# https://archlinux.org")
-	fmt.Println("# https://archlinux.org/mirrors")
-	fmt.Println("# https://archlinux.org/download")
+	fmt.Println("# https://archlinuxarm.org")
 	fmt.Println("# https://en.wikipedia.org/wiki/Arch_Linux")
+	fmt.Println("# https://en.wikipedia.org/wiki/Arch_Linux_ARM")
 	fmt.Println("# https://distrowatch.com/arch")
 
 	dumpOne("https://mirror.xenyth.net/archlinux/iso/latest")
 
-	// Spit out some more handy links
-	fmt.Println("# https://archlinuxarm.org")
-	fmt.Println("# https://archlinuxarm.org/about/downloads")
-	fmt.Println("# https://archlinuxarm.org/platforms/armv8/broadcom/raspberry-pi-4")
-	fmt.Println("# https://en.wikipedia.org/wiki/Arch_Linux_ARM")
-
-	// Get the Pi4 image too
-	/*
-		now := time.Now()
-		fmt.Println("http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz")
-		fmt.Println("	dir=Linux/Arch")
-		fmt.Printf("	out=ArchLinuxARM-rpi-aarch64-%s.tar.gz", now.Format("2006-01-02"))
-		fmt.Println("http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz.md5")
-		fmt.Println("	dir=Linux/Arch")
-		fmt.Printf("	out=ArchLinuxARM-rpi-aarch64-%s.tar.gz.md5", now.Format("2006-01-02"))
-		fmt.Println("http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz.sig")
-		fmt.Println("	dir=Linux/Arch")
-		fmt.Printf("	out=ArchLinuxARM-rpi-aarch64-%s.tar.gz.sig", now.Format("2006-01-02"))
-	*/
-
 	// Get the signing keys too
 	fmt.Println("https://keys.openpgp.org/vks/v1/by-fingerprint/3E80CA1A8B89F69CBA57D98A76A5EF9054449A5C")
 	fmt.Println("	dir=Linux/Arch")
+	fmt.Println("	out=3E80CA1A8B89F69CBA57D98A76A5EF9054449A5C.pgp.asc")
 	fmt.Println("https://keys.openpgp.org/vks/v1/by-fingerprint/68B3537F39A313B3E574D06777193F152BDBE6A6")
 	fmt.Println("	dir=Linux/Arch")
+	fmt.Println("	out=68B3537F39A313B3E574D06777193F152BDBE6A6.pgp.asc")
 }
 
 func dumpOne(url string) {
@@ -81,7 +63,7 @@ func dumpOne(url string) {
 	doc.Find("div.name a").Each(func(i int, s *goquery.Selection) {
 		href, ok := s.Attr("href")
 		if ok {
-			if strings.Contains(href, "archlinux") && !strings.Contains(href, "archlinux-x86_64") && !strings.Contains(href, "archlinux-bootstrap-x86_64") && !strings.Contains(href, ".torrent") {
+			if strings.Contains(href, "archlinux") && !strings.Contains(href, "archlinux-x86_64") && !strings.Contains(href, "archlinux-bootstrap") && !strings.Contains(href, ".torrent") {
 				fmt.Println(fmt.Sprintf("%s/%s", url, href))
 				fmt.Println("	dir=Linux/Arch")
 				if reg.FindString(href) != "" {
