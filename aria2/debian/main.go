24cdc477288dc6a7f45ad31e481380cd9f0b0fed
Tyler Tidman <tyler.tidman@draak.ca>
2026-01-11 20:28:45 -0500

    Spit out variant for Debian SHASUMS files

--- aria2/debian/main.go
+++ aria2/debian/main.go
@@ -95,7 +95,7 @@ func dumpOne(url string, variant string) {
 			if strings.Contains(href, "SHA") && !strings.Contains(href, "SHA1SUMS") {
 				fmt.Println(fmt.Sprintf("%s/%s", url, href))
 				fmt.Println("	dir=Linux/Debian")
-				fmt.Println(fmt.Sprintf("	out=debian-%s-%s", ver, href))
+				fmt.Println(fmt.Sprintf("	out=debian-%s-%s-%s", ver, variant, href))
 			}
 		}
 	})
